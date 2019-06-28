c7d274a430868e8e71ec98ae2727154403b45311
Temitope <temmieayodele@gmail.com>
2019-06-28 23:50:35 +0100

    added render methods

--- src/Component/UserProfile.js
+++ src/Component/UserProfile.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class UserProfile extends Component {
   state = {
-    userInfo: {}
+    userInfo: null
   };
   componentDidMount() {
     async function getData() {
       let res = await fetch("https://api.randomuser.me/");
       let data = await res.json();
-      console.log(data.results)
+      console.log(data.results[0]);
       return data.results[0];
     }
     getData().then(data => {
@@ -17,11 +17,42 @@ class UserProfile extends Component {
       });
     });
   }
-  renderNewUser(){
-      console.log(this.state)
+  renderNewUser() {
+    const {
+      gender,
+      name,
+      location,
+      email,
+      login,
+      dob,
+      phone
+    } = this.state.userInfo;
+    return (
+      <main>
+        <header>
+          <h1>Welcome</h1>
+        </header>
+        <section>
+          Name: {name.first}, {name.last} <br />
+          Gender: {gender} <br />
+          Address: {location.street}, {location.city}, {location.state} <br />
+          Email: {email} <br />
+          Username:{login.username} <br />
+          Date of Birth:{dob.date} <br />
+          PhoneNumber:{phone} <br />
+        </section>
+      </main>
+    );
+  }
+  renderLoading() {
+    return <div>Loading...</div>;
   }
   render() {
-    return <div>{this.renderNewUser()}</div>;
+    return (
+      <div>
+        {this.state.userInfo ? this.renderNewUser() : this.renderLoading()}
+      </div>
+    );
   }
 }
 
